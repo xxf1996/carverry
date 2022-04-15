84970709cce6bc26b5e82d4ac355dc86c4eccb7c
xiexuefeng <1158231926@qq.com>
2022-04-15 17:01:38 +0800

    fix(core): 调整包名映射

--- packages/core/plugins/component-meta.ts
+++ packages/core/plugins/component-meta.ts
@@ -11,10 +11,12 @@ import { Nullable } from '@carverry/app/src/typings/common';
 const require = createRequire(import.meta.url);
 /** 物料包信息，key为包名，value为包的中文名称 */
 const MATERIAL_PACKAGE: Record<string, string> = {
-  'carverry-material': '示例物料包',
+  '@carverry/demo-material': '示例物料包',
   'carverry-404': '不存在的包',
   'carverry-element-plus': 'Element Plus组件',
 };
+
+// TODO: 支持从项目配置文件中增加自定义物料包（包括私库）
 
 function getComponentTree(files: string[], rootDir: string): ComponentTree {
   const root: ComponentTree = {
