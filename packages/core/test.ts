daafb5439bac62b141393a0767f787e20ad7c0be
xiexuefeng <1158231926@qq.com>
2022-09-27 14:38:03 +0800

    refactor(core): 剔除无用代码
    affects: @carverry/core

--- packages/core/test.ts
+++
@@ -1,7 +0,0 @@
-import { runCli, parseNr } from '@antfu/ni';
-
-process.argv.push('-C', '/Users/xuefengxie/Desktop/project/yisight-fe', 'cr'); // -C可以指定cwd；https://github.com/antfu/ni#change-directory
-
-console.log(process.argv);
-
-runCli(parseNr);
